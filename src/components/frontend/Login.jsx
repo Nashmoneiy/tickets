c5ad924b0265d1c391b81a14b5d5009654789f75
Nashmoneiy <107885226+Nashmoneiy@users.noreply.github.com>
2025-06-27 11:23:59 +0100

    initial commit

--- src/components/frontend/Login.jsx
+++ src/components/frontend/Login.jsx
@@ -41,10 +41,7 @@ const Login = () => {
     };
 
     axios
-      .post(
-        `https://ticket-api-production-3c8e.up.railway.appr/api/login`,
-        data
-      )
+      .post(`https://ticket-api-production-3c8e.up.railway.app/api/login`, data)
       .then((response) => {
         if (response.status === 200) {
           const now = new Date().getTime(); // current time in ms
